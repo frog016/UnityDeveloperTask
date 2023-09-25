d556ee6ecf518543c9e0167276f4f7aeb90672aa
frog016 <mixalyov.nikita@mail.ru>
2023-09-25 16:07:37 +0500

    UI
    I made a blank ui window for inventory and items.

---
+++ Assets/1. Vendor Task/Scripts/UI/ItemSlot.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace VendorTask.UI
+{
+    public class ItemSlot : MonoBehaviour
+    {
+        private Transform _content;
+
+        public void SetContent(Transform content)
+        {
+            _content = content;
+            _content.SetParent(transform);
+        }
+
+        public void RemoveContent()
+        {
+            if (_content == null)
+                return;
+
+            _content.SetParent(null);
+        }
+    }
+}
